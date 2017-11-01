5b2e9e13396f4913b7aff0b6b3c2b89a2056d09f
Vassilis Angelis <zonev@Vassiliss-iMac.home>
2017-11-01 20:51:28 +0000

    Fixed Whitenoise Typos

--- Directory/wsgi.py
+++ Directory/wsgi.py
@@ -15,4 +15,4 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Directory.settings")
 
 application = get_wsgi_application()
-application=DjangoWhitNoise(application)
+application=DjangoWhiteNoise(application)
